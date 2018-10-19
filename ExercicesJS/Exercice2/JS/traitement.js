89a06a7e749adf160944dff8fd7091e25a1ad896
Sachtika <tresa1831788>
2018-10-19 13:44:46 -0400

    fini

---
+++ ExercicesJS/Exercice2/JS/traitement.js
@@ -0,0 +1,10 @@
+function btnCalculer_onclick()
+{
+    var MoyKmMois, TotKm;
+
+    MoyKmMois = parseFloat(document.getElementById("txtMot").value);
+    TotKm=MoyKmMois*60;
+
+    console.log("Le total de km calulé pour 5 ans donne: " + TotKm + " Km.");
+
+}
