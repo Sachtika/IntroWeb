d9ff403b5b97a05387eda1c8e98eef18476f4e3a
Sachtika <tresa1831788>
2018-10-19 13:40:32 -0400

    fini

--- ExercicesJS/Exercice1/JS/traitement.js
+++ ExercicesJS/Exercice1/JS/traitement.js
@@ -1,5 +1,10 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var PoidsLivre, Kg;
+
+    PoidsLivre = parseFloat(document.getElementById("txtMot").value);
+ 	Kg=(PoidsLivre)/2.2;
+
+    console.log("Le poids en lb convertit en Kg donne: " + Kg + " Kg.");
 
 }
