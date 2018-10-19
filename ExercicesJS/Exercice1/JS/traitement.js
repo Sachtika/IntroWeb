8e30b916048e4176649ce4f99a5a8e79a2a56e98
Sachtika <tresa1831788>
2018-10-19 13:56:02 -0400

    fini

--- ExercicesJS/Exercice1/JS/traitement.js
+++ ExercicesJS/Exercice1/JS/traitement.js
@@ -2,7 +2,7 @@ function btnCalculer_onclick()
 {
     var PoidsLivre, Kg;
 
-    PoidsLivre = parseFloat(document.getElementById("txtMot").value);
+    PoidsLivre = parseFloat(document.getElementById("txtLb").value);
  	Kg=(PoidsLivre)/2.2;
 
     console.log("Le poids en lb convertit en Kg donne: " + Kg + " Kg.");
