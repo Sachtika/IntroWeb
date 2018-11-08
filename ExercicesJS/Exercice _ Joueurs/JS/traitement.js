acfb3f943243b71319a646c286406b075a7759bf
Sachtika <tresa1831788>
2018-11-08 11:02:17 -0500

    fonctions ajouter

---
+++ ExercicesJS/Exercice _ Joueurs/JS/traitement.js
@@ -0,0 +1,47 @@
+function btnRechercherJoueur_onclick()
+{
+
+
+
+
+    document.getElementById("lblReponse").innerHTML="";
+
+}
+
+function btnAjouter_onclick()
+{
+
+
+
+
+    document.getElementById("lblReponse").innerHTML="";
+}
+
+
+function btnAfficherMoyenne_onclick()
+{
+
+
+
+
+    document.getElementById("lblReponse").innerHTML="";
+}
+
+function AfficherMeilleurPointage_onclick()
+{
+
+
+
+
+    document.getElementById("lblReponse").innerHTML="";
+}
+
+function AfficherPirePointage_onclick()
+{
+
+
+
+
+
+    document.getElementById("lblReponse").innerHTML="";
+}
